2a4d5aff3db189e2b27e542fab14e9997184ba3f
Nishanali3 <nishanaliabdiashim757@gmail.com>
2024-08-09 17:42:39 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
+
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
