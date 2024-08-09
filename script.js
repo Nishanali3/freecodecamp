cd51bedc979075dc5bb608c30b23cadcc6e9009e
Nishanali3 <nishanaliabdiashim757@gmail.com>
2024-08-10 05:08:10 +0600

    step 82

--- script.js
+++ script.js
@@ -219,3 +219,37 @@ for (let i = 1; i < count; i++) {
     rows.push(padRow(i, count));
 }
 
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+}
+
+
+for (let i = 1; i <= count; i++) {
+    // comment
+    rows.push(padRow(i, count));
+}
+
+
+/*
+for (let i = 1; i <= count; i++) {
+ rows.push(padRow(i, count));
+}  */
+
+
+if (true) {
+    console.log("Condition is true");
+}
+
+if (false) {
+    console.log("Condition is true");
+}
+
+if ("false") {
+    console.log("Condition is true");
+}
+
+if ("") {
+    console.log("Condition is true");
+}
+
+
