4d5c808675dfb2228beba3387dc330942c1543ac
Nishanali3 <nishanaliabdiashim757@gmail.com>
2024-08-09 19:40:29 +0600

    script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log(age);
 
 let character = 'Hello';
 let count = 8;
-// console.log(count + 1);
+// console.log(count + 1); 
 // console.log(count - 1);
 // console.log(count * 1);
 // console.log(count / 1);
